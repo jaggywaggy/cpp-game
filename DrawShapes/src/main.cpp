199761245a546395ab40877f7d983e2c6112d99d
Caleb <cjaggard2@gmail.com>
2024-02-12 21:07:11 +1300

    Added RenderShape class

--- DrawShapes/src/main.cpp
+++ DrawShapes/src/main.cpp
@@ -1,15 +1,61 @@
+/**
+ * Calculate pounds of a circle. sf::CircleShape
+ * x,y          x+2r,y
+ * x,y+2r       x+2r,y+2r
+ *
+ * A rectangle would be essentially the same. sf::RectangleShape?
+ *
+ * circle.setFillColor(sf::Color(r, g, b))
+ *
+ * Rect N X Y SX SY R G B W H
+ * Circ N X Y SX SY R G B Radius
+ */
+
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
+
+class RenderShape {
+  std::vector<std::shared_ptr<sf::Shape>> _shapes;
+  std::vector<sf::Text> _text;
+
+public:
+  RenderShape() { }
+
+  RenderShape(std::vector<std::shared_ptr<sf::Shape>> shapes, std::vector<sf::Text> text)
+    : _shapes(shapes)
+    , _text(text)
+  {
+  }
+
+  void addCircleShape(float radius) {
+    _shapes.push_back(std::make_shared<sf::CircleShape>(radius));
+  }
+
+  void addRectangleShape(float width, float height) {
+    _shapes.push_back(std::make_shared<sf::RectangleShape>(sf::Vector2f(width, height)));
+  }
+
+  void readFromFile(std::string& fileName) {
+    std::ifstream fin(fileName);
+
+  }
+
+  // set shape
+  // get shape
+  // set text
+  // get text
+
+};
 
 int main(int argc, char * argv[]) {
-
   const int height = 1280;
   const int width = 720;
-
   sf::RenderWindow window(sf::VideoMode(height, width), "My Window");
   window.setVerticalSyncEnabled(true);
   window.setFramerateLimit(60);
+
 
   while(window.isOpen()) {
 
